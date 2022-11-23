b5563d3265d9053fa5071832d3a110c6c982f65c
Mahmoud Ayman <mahmoudk1000@gmail.com>
2022-11-23 21:02:11 +0200

    x: smoother X

--- modules/x/xinitrc.nix
+++ modules/x/xinitrc.nix
@@ -4,6 +4,7 @@
     home.file.".xinitrc".text = ''
         [ -r ~/.Xmodmap ] && xmodmap ~/.Xmodmap
         xrdb -load $HOME/.Xresources
+        xset r rate 150 50
         $HOME/.fehbg
 
         # Session
