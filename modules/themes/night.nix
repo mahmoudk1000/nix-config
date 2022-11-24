bbb93ec03d03a79e9e8798c353ea1ea26f9cef6e
Mahmoud Ayman <mahmoudk1000@gmail.com>
2022-11-24 10:05:08 +0200

    New colorscheme (WIP)

--- modules/themes/night.nix
+++ modules/themes/night.nix
@@ -1,14 +1,17 @@
 {
-    base00 = "#11171D";
-    base01 = "#E7EFF5";
-    base02 = "#161D25";
-    base03 = "#F07178";
-    base04 = "#13ca91";
-    base05 = "#FFF1AC";
-    base06 = "#7898FB";
-    base07 = "#D7C8F3";
-    base08 = "#A0D6DA";
-    base09 = "#1D2832";
+    base00 = "#13191E";
+    base01 = "#E7E8E9";
+    base02 = "#272d32";
+
+    base03 = "#F6E1DA";
+    base04 = "#DAF6E1";
+    base05 = "#F3F6DA";
+    base06 = "#DAE1F6";
+    base07 = "#E9DAF6";
+    base08 = "#DAF6F1";
+
+    base09 = "#3b4146";
+    
     base0A = "#FFC4BF";
     base0B = "#DAFBDF";
     base0C = "#FFFBB6";
