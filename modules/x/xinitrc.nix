1aac5beee16cc0245c01d1d3d6f2d12bbf91bbdb
Mahmoud Ayman <mahmoudk1000@gmail.com>
2022-11-28 16:33:47 +0200

    dbus

--- modules/x/xinitrc.nix
+++ modules/x/xinitrc.nix
@@ -2,6 +2,14 @@
 
 {
     home.file.".xinitrc".text = ''
+        if test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+            eval $(dbus-launch --exit-with-session --sh-syntax)
+        fi
+        systemctl --user import-environment DISPLAY XAUTHORITY
+
+        if command -v dbus-update-activation-environment >/dev/null 2>&1; then
+            dbus-update-activation-environment DISPLAY XAUTHORITY
+        fi
         [ -r ~/.Xmodmap ] && xmodmap ~/.Xmodmap
         xrdb -load $HOME/.Xresources
         xset r rate 150 50
