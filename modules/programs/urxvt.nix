98363b96d7da0dd0af353aa8b55c0334eb269f07
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-09 22:36:21 +0200

    urxvt: revamp

--- modules/programs/urxvt.nix
+++ modules/programs/urxvt.nix
@@ -3,32 +3,35 @@
 {
     programs.urxvt = {
         enable = true;
-        package = pkgs.rxvt-unicode-unwrapped;
+        package = pkgs.rxvt-unicode;
         iso14755 = true;
-        fonts = [
-            "xft:Iosevka Term:style=Regular:pixelsize=12:antialias=true:autohint=true"
-            "xft:Iosevka Term:style=Bold Italic:pixelsize=12:antialias=true:autohint=true"
-        ];
+        fonts = [ "xft:Iosevka Term:pixelsize=12:antialias=true:autohint=true" ];
         keybindings = {
             "Shift-Up" = ''command:\033]720;1\007'';
             "Shift-Down" = ''command:\033]721;1\007'';
-            "C-Up" = "perl:font-size:increase";
-            "C-Down" = "perl:font-size:decrease";
             "C-equal" = "perl:font-size:reset";
+            "C-plus" = "perl:font-size:increase";
+            "C-minus" = "perl:font-size:decrease";
+            "C-Up" = ''\033[1;5A'';
+            "C-Down" = ''\033[1;5B'';
             "C-Left" = ''\033[1;5D'';
             "C-Right" = ''\033[1;5C'';
             "M-c" = "eval:selection_to_clipboard";
             "M-v" = "eval:paste_clipboard";
         };
         extraConfig = {
+            "depth" = 32;
             "letterSpace" = 0;
             "lineSpace" = 0;
-            "geometry" = "92x24";
+            "geometry" = "70x35";
             "internalBorder" = 25;
             "cursorBlink" = true;
             "cursorUnderline" = false;
+            "URxvt.thickness" = 5;
+            "URxvt.titleBar" = false;
+            "URxvt.intensityStyles" = false;
 
-            "perl-ext-common" = "default,matcher,font-size,clipboard,url-select";
+            "perl-ext-common" = "default,matcher,font-size,clipboard";
             "url-launcher" = "firefox";
             "matcher.button" = 1;
         };
