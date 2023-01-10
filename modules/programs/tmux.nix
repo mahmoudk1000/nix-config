951306fb46cc36b9232efbe0c4b81fe8560cb64c
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-10 22:20:53 +0200

    tmux: done

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -7,7 +7,7 @@
         baseIndex = 1;
         escapeTime = 20;
         shell = "${pkgs.zsh}/bin/zsh";
-        terminal = "screen-256color";
+        terminal = "rxvt-unicode-256color";
         keyMode = "vi";
         plugins = with pkgs; [
             tmuxPlugins.fingers
@@ -15,6 +15,8 @@
             tmuxPlugins.better-mouse-mode
         ];
         extraConfig = ''
+            set -ag terminal-overrides ",rxvt-unicode-256color:RGB"
+            set -g -a terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
             bind q kill-pane
             bind s set status
             bind-key C-t send-prefix
@@ -26,14 +28,14 @@
 
             # pane binds
             unbind-key E
+            bind-key -r C-k resize-pane -U 5
+            bind-key -r C-j resize-pane -D 5
+            bind-key -r C-h resize-pane -L 5
+            bind-key -r C-l resize-pane -R 5
+            bind-key k select-pane -U
+            bind-key j select-pane -D
             bind-key h select-pane -L
-            bind-key n select-pane -D
-            bind-key e select-pane -U
-            bind-key i select-pane -R
-            bind-key -r H resize-pane -L 5
-            bind-key -r N resize-pane -D 5
-            bind-key -r E resize-pane -U 5
-            bind-key -r I resize-pane -R 5
+            bind-key l select-pane -R
 
             # window binds
             bind -n M-j  previous-window
@@ -45,13 +47,12 @@
             bind-key ] swap-window -t +1
             bind-key [ swap-window -t -1
             bind-key c copy-mode
+            bind-key -r "<" swap-window -d -t -1
+            bind-key -r ">" swap-window -d -t +1
 
             # statusline hide / unhide
             bind -n M-down set -q status off
             bind -n M-up set -q status on
-
-            bind-key -r "<" swap-window -d -t -1
-            bind-key -r ">" swap-window -d -t +1
 
             bind-key -T copy-mode-vi v send-keys -X begin-selection
             bind-key -T copy-mode-vi y send-keys -X copy-pipe "xclip -i -sel p -f | xclip -i -sel c "
@@ -66,19 +67,19 @@
             set-option -g status-justify absolute-centre
             set-option -g status-position bottom
 
-            set -g window-status-format         "#[fg=${theme.base06}]#[fg=${theme.base00}]#[bg=${theme.base06}]#I #[bg=${theme.base09}]#[fg=${theme.base01}] #W#[fg=${theme.base09}]#[bg=${theme.base00}]  "
-            set -g window-status-current-format "#[fg=${theme.base08}]#[fg=${theme.base00}]#[bg=${theme.base08}]#I #[bg=${theme.base09}]#[fg=${theme.base01}] #W#[fg=${theme.base09}]#[bg=${theme.base00}]  "
+            set -g window-status-format         "#[fg=cyan,bg=default]#[fg=black,bg=cyan]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]  "
+            set -g window-status-current-format "#[fg=blue,bg=default]#[fg=black,bg=blue]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]  "
 
             set -g status-style "bg=${theme.base00}"
             set -ag status-style "fg=${theme.base01}"
 
             # status right
             set -g status-right-length 70
-            set -g status-right "#[fg=${theme.base07}]█#[fg=${theme.base01}]#[bg=${theme.base09}] %a, %d %b #[fg=${theme.base03}]#[bg=${theme.base09}]•#[fg=${theme.base07}]#[bg=${theme.base09}] %R#[fg=${theme.base09}]#[bg=${theme.base00}]  "
+            set -g status-right "#[bg=default,fg=magenta]█#[fg=white,bg=brightblack] %a, %d %b #[fg=black,bg=brightblack]• #[fg=magenta,bg=brightblack]%R#[fg=brightblack,bg=default]"
 
             # status left
             set -g status-left-length 70
-            set -g status-left "#[fg=${theme.base03}]█#[fg=${theme.base01}]#[bg=${theme.base09}] #(mpc current | sed 's/-/~/')#[fg=${theme.base09}]#[bg=${theme.base00}]  "
+            set -g status-left "#[bg=default,fg=red]█#[fg=white,bg=brightblack] #(mpc current | sed 's/-/~/')#[fg=brightblack,bg=default]"
         '';
     };
 }
