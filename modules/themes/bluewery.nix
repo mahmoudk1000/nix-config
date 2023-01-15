cd193241dcc96dff4309c710efafa4c0375583b1
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-15 22:41:15 +0200

    theme: introduce bluewery

---
+++ modules/themes/bluewery.nix
@@ -0,0 +1,18 @@
+{
+    base00 = "#000f1f";
+    base01 = "#c4c7c7";
+    base02 = "#001E3E";
+    base03 = "#fc6195";
+    base04 = "#5AC6A1";
+    base05 = "#fff1ac";
+    base06 = "#296873";
+    base07 = "#85919b";
+    base08 = "#04a7a7";
+    base09 = "#192635";
+    base0A = "#fc81a5";
+    base0B = "#233439";
+    base0C = "#DF7353";
+    base0D = "#6FaEaF";
+    base0E = "#364f6b";
+    base0F = "#94e7e7";
+}
--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -1,5 +1,6 @@
 let
     blaulicht = import ./blaulicht.nix;
+    bluewery = import ./bluewery.nix;
     coral = import ./coral.nix;
 in
 blaulicht
