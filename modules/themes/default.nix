157fb658cc3f27df17e8501975dcee753cddfaf9
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-15 22:52:23 +0200

    theme: introduce mountain

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -3,6 +3,7 @@ let
     bluewery = import ./bluewery.nix;
     coral = import ./coral.nix;
     iceberg = import ./iceberg.nix;
+    mountain = import ./mountain.nix;
     tokyonight = import ./tokyonight.nix;
 in
 blaulicht
