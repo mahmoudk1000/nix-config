752f722c4c1b63ac7b2a4fe783c215bbc05d096f
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-15 23:05:56 +0200

    theme: introduce bliss

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -1,5 +1,6 @@
 let
     blaulicht = import ./blaulicht.nix;
+    bliss = import ./bliss.nix;
     bluewery = import ./bluewery.nix;
     coral = import ./coral.nix;
     iceberg = import ./iceberg.nix;
