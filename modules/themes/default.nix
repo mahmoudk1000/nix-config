49f8b9d5b3c9be00fe3381c0a33fcfeb17aa8e57
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-15 22:49:49 +0200

    theme: introduce tokyonight

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -3,5 +3,6 @@ let
     bluewery = import ./bluewery.nix;
     coral = import ./coral.nix;
     iceberg = import ./iceberg.nix;
+    tokyonight = import ./tokyonight.nix;
 in
 blaulicht
---
+++ modules/themes/tokyonight.nix
@@ -0,0 +1,18 @@
+{
+    base00 = "#1a1b26";
+    base01 = "#a9b1d6";
+    base02 = "#24283b";
+    base03 = "#f7768e";
+    base04 = "#9ece6a";
+    base05 = "#e0af68";
+    base06 = "#7aa2f7";
+    base07 = "#bb9af7";
+    base08 = "#7dcfff";
+    base09 = "#414868";
+    base0A = "#ff9e64";
+    base0B = "#9ece6a";
+    base0C = "#e0af68";
+    base0D = "#7aa2f7";
+    base0E = "#bb9af7";
+    base0F = "#7dcfff";
+}
