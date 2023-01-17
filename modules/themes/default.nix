92bdae3aa0a0fc5845359c2c300f10036d7516a6
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-17 20:43:16 +0200

    theme: introduce kalt

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -4,6 +4,7 @@ let
     bluewery = import ./bluewery.nix;
     coral = import ./coral.nix;
     iceberg = import ./iceberg.nix;
+    kalt = import ./kalt.nix;
     late = import ./late.nix;
     mountain = import ./mountain.nix;
     night = import ./night.nix;
