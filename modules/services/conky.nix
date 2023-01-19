b94aee1587d3df639fb49ef31d20401a8419f2e6
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-01-19 20:57:00 +0200

    conky: nix-ed
    I need to fix this weather decription not capital case at start. I already tried `sed -r 's/[^ ]*/\u&/g'` but IDK why the whole text disappear, when it there. Despite it works on termnal. ;\

---
+++ modules/services/conky.nix
@@ -0,0 +1,77 @@
+{ config, pkgs, theme, ...  }:
+
+let
+    apiKey = "a89384ad87d37345cca9848d9e0b477f";
+    cityId = "361058";
+    url = ''"api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&cnt=5&units=metric&lang=en"'';
+
+    klima = pkgs.writeScriptBin "klima" ''
+        ${pkgs.curl} -s ${url} -o ~/.cache/weather.json
+    '';
+
+    conky-config = pkgs.writeText "conky-config" ''
+        conky.config = {
+        -- Size and Position settings --
+            alignment = 'top_left',
+            gap_x = 48,
+            gap_y = 35,
+            maximum_width = 400,
+            minimum_height = 70,
+            minimum_width = 420,
+        -- Text settings --
+            use_xft = true,
+            font = 'Iosevkat:size=9',
+        -- Color Settings --
+            default_color = '${theme.base09}',
+            color1 = '${theme.base06}',
+        -- Window Settings --
+            background = false,
+            border_width = 0,
+            draw_shades = false,
+            own_window = true,
+            own_window_colour = '000000',
+            own_window_class = 'Conky',
+            own_window_argb_visual = false,
+            own_window_type = 'desktop',
+            own_window_transparent = true,
+            own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
+            stippled_borders = 0,
+        -- Others --
+            double_buffer = true,
+            extra_newline = false,
+            update_interval = 1,
+        }
+        conky.text = [[
+        ''${execi 600 ${klima}/bin/klima}\
+        ''${offset 0}''${voffset 0}''${font Iosevka:style=Heavy Extended:size=35}''${color}''${time %H.%M} \
+        ''${offset -18}''${voffset -20}''${color}''${font Iosevka:style=Heavy Extended:size=11}''${execi 1 ${pkgs.mpc-cli}/bin/mpc current | ${pkgs.gnused}/bin/sed -r 's/ - /\n/g' | ${pkgs.coreutils}/bin/head -1}
+        ''${offset 150}''${voffset -3}''${color}''${font Iosevka:style=Heavy Extended:size=13}''${execi 1 ${pkgs.mpc-cli}/bin/mpc current | ${pkgs.gnused}/bin/sed -r 's/ - /\n/g' | ${pkgs.coreutils}/bin/tail -1}
+        ''${offset 0}''${voffset 5}''${font Iosevka:style=Heavy Extended:size=13}''${color1}''${execi 600 ${pkgs.coreutils}/bin/cat ~/.cache/weather.json | ${pkgs.jq}/bin/jq '.main.temp'}°C\
+        ''${offset 10}''${voffset 0}''${font Iosevka:style=Heavy Extended:size=13}''${color}''${execi 600 ${pkgs.coreutils}/bin/cat ~/.cache/weather.json | ${pkgs.jq}/bin/jq -r '.weather[0].description'}
+        ]]
+    '';
+in
+{
+    config = {
+        systemd.user.services.conky = {
+        Unit = {
+            Description = "Conky System Monitor";
+            PartOf = "graphical-session.target";
+        };
+        Service = {
+            Type = "forking";
+            ExecStart = ''
+                ${pkgs.bash}/bin/bash -c '${pkgs.conky}/bin/conky --daemonize -c ${conky-config}'
+            '';
+            OOMPolicy = "kill";
+        };
+    };
+    systemd.user.timers.conky = {
+        Timer = {
+            OnActiveSec = "10s";
+            AccuracySec = "1s";
+        };
+        Install = { WantedBy = [ "graphical-session.target" ]; };
+        };
+    };
+}
--- modules/services/default.nix
+++ modules/services/default.nix
@@ -4,6 +4,7 @@
     imports = [
         ./autorandr.nix
         ./betterlockscreen.nix
+        ./conky.nix
         ./dunst.nix
         ./flameshot.nix
         ./keyring.nix
