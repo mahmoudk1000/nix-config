d64f3dc824ae8cfcbd960a816ef6f8498f5d3a28
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-02-05 13:05:27 +0200

    rofi nix-ed poww
    Nixed poww (rofi power menu).

--- modules/programs/rofi.nix
+++ modules/programs/rofi.nix
@@ -36,10 +36,10 @@ in
                 padding = mkLiteral "5px 5px 5px 5px";
             };
             prompt = {
-                font = mkLiteral ''"IosevkaTerm Nerd Font 12"'';
+                font = mkLiteral ''"IosevkaTerm Nerd Font 10"'';
                 background-color = mkLiteral "@al";
                 vertical-align = mkLiteral "0.5";
-                padding = mkLiteral "0.6em 0.75em";
+                padding = mkLiteral "0.6em 1.1em 0.6em 0.75em";
                 border-radius = mkLiteral "4px";
             };
             inputbar = {
@@ -73,4 +73,58 @@ in
             };
         };
     };
+
+    home.file.".config/rofi/poww.rasi".text = ''
+        configuration {
+	    font:   "Material Design Icons Desktop 20";
+	    disable-history:    false;
+	    sidebar-mode:   false;
+	    show-icons:    false;
+        }
+        @theme "/dev/null"
+        * {
+            bg:	    ${theme.base00};
+            lg:	    ${theme.base02};
+            fg:	    ${theme.base01};
+            se:     ${theme.base06};
+            background-color:   @bg;
+            text-color:     @fg;
+        }
+        window {
+            transparency:   "real";
+            border-radius:  4px;
+            location:   center;
+            width:      400px;
+        }
+        listview {
+            columns:	5;
+            lines:	1;
+            spacing:    12px;
+            cycle:      true;
+            dynamic:    true;
+            layout:     vertical;
+        }
+        mainbox {
+            children:	[ listview ];
+            spacing:	20px;
+            margin:	20px;
+        }
+        element {
+            background-color:   @lg;
+            border-radius:      4px;
+        }
+        element-text {
+            background-color:   inherit;
+            text-color:       	inherit;
+            expand:             true;
+            horizontal-align:   0.5;
+            vertical-align:     0.5;
+            margin:             15px 15px 15px 15px;
+        }
+        element selected {
+            text-color: 	@bg;
+            background-color:   @se;
+            border-radius:      4px;
+        }
+    '';
 }
