6a691cb767d39a4f43cf25b88be390c1d47e72df
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-02-07 15:41:57 +0200

    zsh: rewrite

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -19,32 +19,41 @@
             autocd = true;
             completionInit = "autoload -U compinit && compinit";
             historySubstringSearch.enable = true;
-            history.size = 10000;
+            history = {
+                path = "$HOME/.zsh_history";
+                size = 10000;
+                share = true;
+                extended = true;
+                ignoreSpace = true;
+                ignoreDups = true;
+                expireDuplicatesFirst = true;
+                ignorePatterns = [ "ls" "rm" "kill" "exit" "pkill" ];
+            };
             shellAliases = {
-                zsh = "exec zsh";
-                py = "python3";
+                zsh  = "exec zsh";
+                py   = "python3";
                 open = "xdg-open";
                 free = "free -h";
 
-                ".." = "cd ..";
+                ".."  = "cd ..";
                 "..." = "cd ../..";
-                l = "ls --color=auto";
-                ll = "ls -lh --color=auto";
-                la = "ls -alh --color=auto";
+                l     = "ls --color=auto";
+                ll    = "ls -lh --color=auto";
+                la    = "ls -alh --color=auto";
 
-                v = "nvim";
+                v  = "nvim";
                 sv = "sudo nvim";
 
-                g = "git";
-                ga = "git add";
+                g   = "git";
+                ga  = "git add";
                 glg = "git log";
-                gl = "git clone";
-                gs = "git status --short";
+                gl  = "git clone";
+                gs  = "git status --short";
                 gaa = "git add --all";
                 gcp = "git cherry-pick";
                 gcm = "git commit --verbose -m ";
                 gpl = "git pull";
-                gp = "git push";
+                gp  = "git push";
                 gff = "git diff --minimal";
 
                 nc = "ncmpcpp";
@@ -53,35 +62,33 @@
             sessionVariables = {
                 "EDITOR" = "nvim";
                 "BROWSER" = "firefox";
-                "HISTFILE" = "$HOME/.zsh_history";
-                "MAKEFLAGS" = " -j 8";
             };
             initExtra = ''
                 autoload -Uz promptinit; promptinit
                 eval "$(starship init zsh)"
             '';
             initExtraBeforeCompInit = ''
-                bindkey '^a' beginning-of-line
-                bindkey '^e' end-of-line
-                bindkey '^w' backward-kill-word
-                bindkey '^f' forward-char
-                bindkey '^b' backward-char
-                bindkey '^r' history-incremental-search-backward
-                bindkey "^[[5~"	  beginning-of-line
-                bindkey "^A"	  beginning-of-line
-                bindkey "^[[6~"	  end-of-line
-                bindkey "^E"	  end-of-line
-                bindkey "^[[2~"   overwrite-mode
-                bindkey "^[[3~"	  delete-char
-                bindkey "^[[1;5C" forward-word
-                bindkey "^[[1;5D" backward-word
+                # Keybinds
+                bindkey '^a'        beginning-of-line
+                bindkey '^e'        end-of-line
+                bindkey '^w'        backward-kill-word
+                bindkey '^f'        forward-char
+                bindkey '^b'        backward-char
+                bindkey '^r'        history-incremental-search-backward
+                bindkey "^[[5~"     beginning-of-line
+                bindkey "^A"	    beginning-of-line
+                bindkey "^[[6~"	    end-of-line
+                bindkey "^E"	    end-of-line
+                bindkey "^[[2~"     overwrite-mode
+                bindkey "^[[3~"	    delete-char
+                bindkey "^[[1;5C"   forward-word
+                bindkey "^[[1;5D"   backward-word
 
                 # Completion
                 zstyle ':completion:*:descriptions' format '%U%B%d%b%u'
                 zstyle ':completion:*:warnings' format '%BSorry, no matches for: %d%b'
                 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
                                             /usr/sbin /usr/bin /sbin /bin /usr/X11R6/bin
-
                 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
                 zstyle ':completion:*' use-cache on
                 zstyle ':completion:*' cache-path ~/.zsh_cache
@@ -93,22 +100,16 @@
                 zstyle ':completion:*' menu select
 
                 setopt APPEND_HISTORY
-                setopt EXTENDED_HISTORY          # using ":start:elapsed;command" format
-                setopt HIST_EXPIRE_DUPS_FIRST    # remote duplicate when trimming history
-                setopt HIST_IGNORE_DUPS          # ignore duplicated commands
-                setopt HIST_IGNORE_ALL_DUPS
                 setopt HIST_FIND_NO_DUPS
-                setopt HIST_IGNORE_SPACE
                 setopt HIST_SAVE_NO_DUPS
                 setopt HIST_VERIFY
-                setopt INC_APPEND_HISTORY        # write immediately, not on shell exit
-                setopt SHARE_HISTORY             # share command history data
-                setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
+                setopt INC_APPEND_HISTORY
+                setopt HIST_REDUCE_BLANKS
 
-                # Custom Highlight syntax
-                ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=${theme.base09}" #  underline for underline
+                # Custom Highlight Syntax
+                ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=${theme.base09}"
 
-                # cmd not found msg
+                # Command Not Found Msg
                 command_not_found_handler() {
                     echo -e "nope, \e[31m'$0'\e[0m didn't work."; return 1
                 }
@@ -118,6 +119,12 @@
                     exec startx
                 fi
             '';
+            dirHashes = {
+                docs  = "$HOME/Documents";
+                vids  = "$HOME/Videos";
+                dl    = "$HOME/Downloads";
+                musk  = "$HOME/Musik";
+            };
         };
     };
 }
