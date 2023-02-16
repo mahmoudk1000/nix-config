29e74aeed9d0f92d540478d5596790274e6ec6a6
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-02-16 19:41:19 +0200

    enable neovim overlay and flak.lock update

--- flake.nix
+++ flake.nix
@@ -4,12 +4,10 @@
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         nur.url = github:nix-community/NUR;
-
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-
         neovim-nightly = {
             url = "github:nix-community/neovim-nightly-overlay";
             inputs.nixpkgs.follows = "nixpkgs";
@@ -20,7 +18,7 @@
         let
             overlays = [
                 inputs.nur.overlay
-                # inputs.neovim-nightly.overlay
+                inputs.neovim-nightly.overlay
             ];
         in
         {
