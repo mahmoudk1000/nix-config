18f1e1af25c854441457c46c9d1b2440f20d8ace
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-03-31 16:50:04 +0200

    tmux: optimization

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -7,16 +7,14 @@
         baseIndex = 1;
         escapeTime = 20;
         shell = "${pkgs.zsh}/bin/zsh";
-        terminal = "rxvt-unicode-256color";
+        terminal = "xterm-256color";
         keyMode = "vi";
-        plugins = with pkgs; [
-            tmuxPlugins.fingers
-            tmuxPlugins.yank
-            tmuxPlugins.better-mouse-mode
+        historyLimit = 10000;
+        plugins = with pkgs.tmuxPlugins; [
+            fingers
+            better-mouse-mode
         ];
         extraConfig = ''
-            set -ag terminal-overrides ",rxvt-unicode-256color:RGB"
-            set -g -a terminal-overrides ',*:Ss=\E[%p1%d q:Se=\E[2 q'
             bind q kill-pane
             bind s set status
             bind-key C-t send-prefix
@@ -67,19 +65,19 @@
             set-option -g status-justify absolute-centre
             set-option -g status-position bottom
 
-            set -g window-status-format         "#[fg=cyan,bg=default]#[fg=black,bg=cyan]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]"
-            set -g window-status-current-format "#[fg=blue,bg=default]#[fg=black,bg=blue]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]"
+            set -g window-status-format         "#[fg=cyan,bg=default]▒▓█#[fg=black,bg=cyan]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]█▓▒"
+            set -g window-status-current-format "#[fg=blue,bg=default]▒▓█#[fg=black,bg=blue]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]█▓▒"
 
             set -g status-style "bg=${theme.base00}"
             set -ag status-style "fg=${theme.base01}"
 
             # status right
             set -g status-right-length 70
-            set -g status-right "#[bg=default,fg=magenta]█#[fg=white,bg=brightblack] %a, %d %b #[fg=black,bg=brightblack]• #[fg=magenta,bg=brightblack]%R#[fg=brightblack,bg=default]"
+            set -g status-right "#[bg=default,fg=magenta]▒▓█#[fg=white,bg=brightblack] %a, %d %b #[fg=black,bg=brightblack]• #[fg=magenta,bg=brightblack]%R#[fg=brightblack,bg=default]█▓▒"
 
             # status left
             set -g status-left-length 70
-            set -g status-left "#[bg=default,fg=red]█#[fg=white,bg=brightblack] #(mpc current | sed 's/-/~/')#[fg=brightblack,bg=default]"
+            set -g status-left "#[bg=default,fg=red]▒▓█#[fg=white,bg=brightblack] #(mpc current | sed 's/-/~/')#[fg=brightblack,bg=default]█▓▒"
         '';
     };
 }
