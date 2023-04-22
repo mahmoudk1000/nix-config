a3b3fedeecc24720dd90f36176c37920861419ca
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-04-22 12:58:42 +0200

    neovim: change hl for gitsigns

--- modules/programs/neovim/init.lua
+++ modules/programs/neovim/init.lua
@@ -244,11 +244,11 @@ require('telescope').setup {
 -- Github
 require('gitsigns').setup {
   signs = {
-    add = { hl = "DiffAdd", text = '🞧', numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "DiffChange", text = '⏺', numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "DiffDelete", text = '🭸', numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "DiffDelete", text = '🭶', numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "DiffChangeDelete", text = '~', numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    untracked = { hl = "GitSignsAdd", text = '🞧', numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    add = { hl = "GitGutterAdd", text = '🞧', numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    change = { hl = "GitGutterChange", text = '⏺', numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitGutterDelete", text = '🭸', numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitGutterDeleteChange", text = '🭶', numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    changedelete = { hl = "GitGutterChange", text = '~', numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    untracked = { hl = "GitGutterAdd", text = '🞧', numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
   },
 }
