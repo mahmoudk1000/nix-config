94ddb2af94f61998f52c4cb6411c1c0ac384af6e
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-06-19 22:38:25 +0300

    Spotify adblocked
    Poor Me!

---
+++ modules/programs/default.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+    imports = [
+        ./ncmpcpp
+        ./neovim/neovim.nix
+        ./2bwm.nix
+        ./accounts.nix
+        ./alacritty.nix
+        ./awesomewm.nix
+        # ./bash.nix
+        ./bspwm.nix
+        ./firefox.nix
+        ./git.nix
+        ./herbstluftwm.nix
+        ./newsboat.nix
+        ./rofi.nix
+        ./spotify.nix
+        ./starship.nix
+        ./thunar.nix
+        ./tmux.nix
+        ./urxvt.nix
+        ./vscode.nix
+        ./zathura.nix
+        ./zsh.nix
+    ];
+}
