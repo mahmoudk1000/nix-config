d9cfe36c76b2ba5180bf208fcb7e9a03ad37c653
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-01-16 18:24:00 +0200

    blaulicht: new black colors

--- modules/themes/blaulicht.nix
+++ modules/themes/blaulicht.nix
@@ -1,14 +1,14 @@
 {
     base00 = "#11171D";
     base01 = "#E7EFF5";
-    base02 = "#1a242d";
+    base02 = "#1a1f26";
     base03 = "#F6BAB5";
     base04 = "#D0f1D5";
     base05 = "#FFF1AC";
     base06 = "#90A8BE";
     base07 = "#D7C8F3";
     base08 = "#A0D6DA";
-    base09 = "#24313d";
+    base09 = "#212832";
     base0A = "#FFC4BF";
     base0B = "#DAFBDF";
     base0C = "#FFFBB6";
