030fe31c95c627ec11595647654c8db10b04f029
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-01-16 18:40:50 +0200

    [TEMP] obsidian: fix for EOL electron 25

--- home-manager/labbi/home.nix
+++ home-manager/labbi/home.nix
@@ -1,5 +1,19 @@
-{ config, pkgs, ... }:
+{ config
+, pkgs
+, lib
+, ...
+}:
 
+let
+  obsidian = lib.throwIf (lib.versionOlder "1.5.3" pkgs.obsidian.version) "Obsidian no longer requires EOL Electron" (
+    pkgs.obsidian.override {
+      electron = pkgs.electron_25.overrideAttrs (_: {
+        preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+        meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
+      });
+    }
+  );
+in
 {
     imports = [
         ../../modules/programs
