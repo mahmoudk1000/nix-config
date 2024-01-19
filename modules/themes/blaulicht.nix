b0b73aa76be91d978d7acda7c8030d78ef2dc26e
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-01-19 15:44:28 +0200

    blaulicht: new black colors was bad idea
    This is a second attempt tho!

--- modules/themes/blaulicht.nix
+++ modules/themes/blaulicht.nix
@@ -1,14 +1,14 @@
 {
     base00 = "#11171D";
     base01 = "#E7EFF5";
-    base02 = "#1a1f26";
+    base02 = "#1a232c";
     base03 = "#F6BAB5";
     base04 = "#D0f1D5";
     base05 = "#FFF1AC";
     base06 = "#90A8BE";
     base07 = "#D7C8F3";
     base08 = "#A0D6DA";
-    base09 = "#212832";
+    base09 = "#222e3a";
     base0A = "#FFC4BF";
     base0B = "#DAFBDF";
     base0C = "#FFFBB6";
