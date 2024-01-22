683f2f934fee0bc0b9dc182678bb9d0c01b3ec8d
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-01-22 23:39:09 +0200

    sound: add bluez codecs
    An attempt to fix audio shuttering in bluetooth audio

--- hosts/labbi/modules/sound.nix
+++ hosts/labbi/modules/sound.nix
@@ -82,6 +82,7 @@ in
         ["bluez5.enable-sbc-xq"] = true,
         ["bluez5.enable-msbc"] = true,
         ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.codecs"] = "[sbc sbc_xq]",
         ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
       }
     '';
