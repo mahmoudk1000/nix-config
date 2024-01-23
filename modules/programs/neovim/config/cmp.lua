beb57dc3ea78fd7ccfc8000d2d176fc518200978
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-01-23 12:15:49 +0200

    neovim: tags source & remove has_words_before()

--- modules/programs/neovim/config/cmp.lua
+++ modules/programs/neovim/config/cmp.lua
@@ -1,12 +1,6 @@
 local cmp     = require("cmp")
 local luasnip = require("luasnip")
 
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-end
-
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -19,8 +13,8 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       else
@@ -42,7 +36,7 @@ cmp.setup({
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
     ["<C-p>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
@@ -72,6 +66,15 @@ cmp.setup({
         enable_in_context = function()
           return true
         end
+      }
+    },
+    {
+      name = 'tags',
+      option = {
+        complete_defer = 100,
+        max_item_count = 10,
+        exact_match = false,
+        current_buffer_only = false,
       }
     },
     { name = "buffer" },
