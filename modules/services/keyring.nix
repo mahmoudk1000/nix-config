924c10591ec170ff1172f9edeabc73a699003509
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-03-22 21:17:55 +0200

    kering: new options and set pinentry package

--- modules/services/keyring.nix
+++ modules/services/keyring.nix
@@ -1,9 +1,16 @@
+{ config
+, pkgs
+, ...
+}:
+
 {
   programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
+    enableZshIntegration = true;
     defaultCacheTtl = 1800;
     enableSshSupport = true;
-    pinentryFlavor = "gnome3";
+    enableExtraSocket = true;
+    pinentryPackage = if config.gtk.enable then pkgs.pinentry-gnome3 else pkgs.pinentry-curses;
   };
 }
