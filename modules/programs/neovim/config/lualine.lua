c1c4ecb75bf7f47f996da1ce5ccde4940532b09b
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-04-08 19:02:39 +0200

    neovim: add icon to mode in lualine

--- modules/programs/neovim/config/lualine.lua
+++ modules/programs/neovim/config/lualine.lua
@@ -1,39 +1,39 @@
 require("lualine").setup({
-  options = {
-    theme = {
-      normal = { a = "LuaLineNormalA", b = "LuaLineNormalB", c = "LuaLineNormalC" },
-      insert = { a = "LuaLineInsertA" },
-      visual = { a = "LuaLineVisualA" },
-      replace = { a = "LuaLineReplaceA" }
-    },
-    icons_enabled = true,
-    always_divide_middle = true,
-    globalstatus = true,
-    component_separators = "|",
-    section_separators = { right = "▒▓█", left = "█▓▒" },
-    disabled_filetypes = {
-      statusline = { "neo-tree" },
-    },
-    ignore_focus = { "neo-tree" },
-    refresh = {
-      statusline = 1000
-    },
-  },
-  sections = {
-    lualine_a = {
-      { "mode", separator = { left = "▒▓█" }, right_padding = 2 },
-    },
-    lualine_b = { { "filename", file_status = false, path = 1 }, { "branch", icon = "󰘬" } },
-    lualine_c = { "diff" },
-    lualine_x = { { "diagnostics", sources = { "nvim_diagnostic" } } },
-    lualine_y = { "%p%%/%L" },
-    lualine_z = {
-      { "location", separator = { right = "█▓▒" }, left_padding = 2 },
-    },
-  },
-  extensions = {
-    "neo-tree",
-    "fugitive",
-    "toggleterm"
-  }
+	options = {
+		theme = {
+			normal = { a = "LuaLineNormalA", b = "LuaLineNormalB", c = "LuaLineNormalC" },
+			insert = { a = "LuaLineInsertA" },
+			visual = { a = "LuaLineVisualA" },
+			replace = { a = "LuaLineReplaceA" },
+		},
+		icons_enabled = true,
+		always_divide_middle = true,
+		globalstatus = true,
+		component_separators = "|",
+		section_separators = { right = "▒▓█", left = "█▓▒" },
+		disabled_filetypes = {
+			statusline = { "neo-tree" },
+		},
+		ignore_focus = { "neo-tree" },
+		refresh = {
+			statusline = 1000,
+		},
+	},
+	sections = {
+		lualine_a = {
+			{ "mode", icons_enabled = true, icon = " ", separator = { left = "▒▓█" }, right_padding = 2 },
+		},
+		lualine_b = { { "filename", file_status = false, path = 1 }, { "branch", icon = "󰘬" } },
+		lualine_c = { "diff" },
+		lualine_x = { { "diagnostics", sources = { "nvim_diagnostic" } } },
+		lualine_y = { "%p%%/%L" },
+		lualine_z = {
+			{ "location", separator = { right = "█▓▒" }, left_padding = 2 },
+		},
+	},
+	extensions = {
+		"neo-tree",
+		"fugitive",
+		"toggleterm",
+	},
 })
