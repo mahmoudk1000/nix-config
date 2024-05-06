9d16928db0636f6d3682cbfc88d2e566d5d0d771
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-05-06 14:53:11 +0300

    vencord: fix & update

--- modules/programs/vencord.nix
+++ modules/programs/vencord.nix
@@ -20,7 +20,7 @@ with theme;
     	/* amount of spacing and padding */
     	--spacing: 12px;
     	/* radius of round corners */
-    	--roundness: 12px;
+    	--roundness: 8px;
 
     	/* font, change to 'gg sans' for default discord font*/
     	--font: 'figtree';
@@ -33,15 +33,15 @@ with theme;
     	--discord-icon: none; /* change to 'block' to show default discord icon */
 
     	/* color of links */
-    	--accent-1: #${base07};
+    	--accent-1: ${base07};
     	/* color of unread dividers and some indicators */
-    	--accent-2: #${base04};
+    	--accent-2: ${base04};
     	/* color of accented buttons */
-    	--accent-3: #${base06};
+    	--accent-3: ${base06};
     	/* color of accented buttons when hovered */
-    	--accent-4: #${base0D};
+    	--accent-4: ${base08};
     	/* color of accented buttons when clicked */
-    	--accent-5: #${base09};
+    	--accent-5: ${base09};
 
     	/* color of mentions and messages that mention you */
     	--mention: hsla(190, 80%, 52%, 0.1);
@@ -49,24 +49,24 @@ with theme;
     	--mention-hover: hsla(190, 80%, 52%, 0.05);
 
     	/* color of bright text on colored buttons */
-    	--text-1: #${base01};
+    	--text-1: ${base01};
     	/* color of headings and important text */
-    	--text-2: #${base01};
+    	--text-2: ${base05};
     	/* color of normal text */
-    	--text-3: #${base01};
+    	--text-3: ${base01};
     	/* color of icon buttons and channels */
-    	--text-4: #${base09};
+    	--text-4: ${base06};
     	/* color of muted channels/chats and timestamps */
-    	--text-5: #${base02};
+    	--text-5: ${base09};
 
     	/* color of dark buttons when clicked */
-    	--bg-1: #${base00};
+    	--bg-1: ${base02};
     	/* color of dark buttons */
-    	--bg-2: #${base02};
+    	--bg-2: ${base09};
     	/* color of spacing around panels and secondary elements */
-    	--bg-3:  #${base02};
+    	--bg-3: ${base00};
     	/* main background color */
-    	--bg-4: #${base00};
+    	--bg-4: ${base02};
 
     	/* color of channels and icon buttons when hovered */
     	--hover: hsla(230, 20%, 40%, 0.1);
