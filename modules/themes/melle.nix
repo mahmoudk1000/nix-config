eb0b2b11e2e62a54fd12ace46dbe6e1d1c62e82a
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-05-26 12:10:04 +0300

    theme: update melle

--- modules/themes/melle.nix
+++ modules/themes/melle.nix
@@ -1,18 +1,18 @@
 {
   base00 = "#11171D";
   base01 = "#E0E0E0";
-  base02 = "#0E1419";
+  base02 = "#0D1217";
   base03 = "#FFB3B3";
   base04 = "#B3FFB3";
   base05 = "#FFFFB3";
-  base06 = "#B3D1FF";
+  base06 = "#80A1C1";
   base07 = "#E3B3FF";
   base08 = "#B3FFFF";
-  base09 = "#0C1115";
+  base09 = "#1A2028";
   base0A = "#FFD6D6";
   base0B = "#D6FFD6";
   base0C = "#FFFFD6";
-  base0D = "#D6E4FF";
+  base0D = "#A9C3DA";
   base0E = "#F0D6FF";
   base0F = "#D6FFFF";
 }
