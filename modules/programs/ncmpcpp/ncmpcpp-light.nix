224036b721c8eada46b3262d76aebb11f8cfd33d
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-06-19 17:17:40 +0300

    ncmpcpp: new item indicators

--- modules/programs/ncmpcpp/ncmpcpp-light.nix
+++ modules/programs/ncmpcpp/ncmpcpp-light.nix
@@ -20,7 +20,7 @@
       allow_for_physical_item_deletion = "no";
       lines_scrolled = "1";
       follow_now_playing_lyrics = "yes";
-      lyrics_fetchers = "musixmatch";
+      lyrics_fetcher = "lyricswiki, lyricscom, lyricsplugin";
 
       visualizer_data_source = "/tmp/mpd.fifo";
       visualizer_output_name = "mpd_visualizer";
@@ -46,10 +46,10 @@
       song_list_format = "$(008)%t$R  $(247)%a$R$5  %l$8";
       song_columns_list_format = "(53)[blue]{tr} (45)[blue]{a}";
 
-      current_item_prefix = "$b$2| ";
+      current_item_prefix = "$b$2‣ ";
       current_item_suffix = "$/b$5";
 
-      now_playing_prefix = ''"$b$5‣ "'';
+      now_playing_prefix = ''"$b$5󰎇 "'';
       now_playing_suffix = "$/b$5";
       song_library_format = "{{%a - %t} (%b)}|{%f}";
 
