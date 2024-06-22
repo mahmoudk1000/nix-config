b3a9c0f2ed0f3016320e5d91d975dd23e4dbe6c1
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-06-22 20:57:20 +0300

    redshift: fix typo

--- modules/services/redshift.nix
+++ modules/services/redshift.nix
@@ -1,7 +1,6 @@
 {
   services.redshift = {
     enable = true;
-    tray = false;
     provider = "manual";
     latitude = 31.2156;
     longitude = 29.9553;
@@ -9,16 +8,16 @@
       day = 6000;
       night = 3500;
     };
-  };
-  settings = {
-    redshift = {
-      gamma = 0.8;
-      gamma-day = 0.9;
-      gamma-night = 0.8;
-      adjustment-method = "randr";
-    };
-    randr = {
-      screen = 0;
+    settings = {
+      redshift = {
+        gamma = 0.8;
+        gamma-day = 0.9;
+        gamma-night = 0.8;
+        adjustment-method = "randr";
+      };
+      randr = {
+        screen = 0;
+      };
     };
   };
 }
