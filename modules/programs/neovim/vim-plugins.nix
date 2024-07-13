a5547484f951f1ca7af57f83bf8f8f75699089ca
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-07-13 20:25:21 +0300

    neovim: (vim-plugin) update markview

--- modules/programs/neovim/vim-plugins.nix
+++ modules/programs/neovim/vim-plugins.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 
 {
-  markview-nvim = pkgs.vimUtils.buildVimPlugin {
+  markview-nvim = pkgs.vimUtils.buildVimPlugin rec {
     pname = "markview-nvim";
-    version = "alpha";
+    version = "${builtins.substring 0 7 src.rev}";
     src = pkgs.fetchFromGitHub {
       owner = "OXY2DEV";
       repo = "markview.nvim";
-      rev = "f60219dce7283192d549f21847fcf8537bf6d260";
-      hash = "sha256-E1lHSjbnOOIeieaFJ+INvxJHCbfVS3mwbQ6wrlKeGSQ=";
+      rev = "f801b3e1213f6b92b9110660e3bcf3af5c5ff758";
+      hash = "sha256-WU2sGOWeI6/Ljc4fpfK9zLCClpPJJ7BqYoixKjScLXE=";
     };
   };
 }
