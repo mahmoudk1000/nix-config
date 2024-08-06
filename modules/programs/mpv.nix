4c741db9f512b2e569bb3b95c56bf800a45fe4f1
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-08-06 14:41:51 +0300

    mpv: use default mpv

--- modules/programs/mpv.nix
+++ modules/programs/mpv.nix
@@ -3,9 +3,6 @@
 {
   programs.mpv = {
     enable = true;
-    package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override { vapoursynthSupport = true; }) {
-      youtubeSupport = true;
-    };
     defaultProfiles = [ "best" ];
     profiles = {
       best = {
