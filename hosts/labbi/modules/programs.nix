80c3159da60452df4c24b7c568cd4220cab172e6
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-08-06 10:37:50 +0300

    zsh: custom command_not_found_handler

--- hosts/labbi/modules/programs.nix
+++ hosts/labbi/modules/programs.nix
@@ -3,7 +3,6 @@
 {
   programs = {
     adb.enable = true;
-    zsh.enable = true;
     xfconf.enable = true;
     dconf.enable = true;
     gnupg.agent = {
@@ -13,6 +12,14 @@
     java = {
       enable = true;
       package = pkgs.jdk;
+    };
+    zsh = {
+      enable = true;
+      interactiveShellInit = ''
+        command_not_found_handler() {
+          echo -e "nope, \e[31m'$0'\e[0m didn't work."; return 1
+        }
+      '';
     };
     command-not-found.enable = false;
   };
