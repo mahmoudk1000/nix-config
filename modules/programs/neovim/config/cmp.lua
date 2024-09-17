2c14d823c1c30e1dbd43effcfe36969c041b92af
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-09-17 10:05:19 +0300

    neovim: update has_words_before

--- modules/programs/neovim/config/cmp.lua
+++ modules/programs/neovim/config/cmp.lua
@@ -31,11 +31,8 @@ local kind_icons = {
 }
 
 local has_words_before = function()
-	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-		return false
-	end
-	local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
@@ -62,7 +59,7 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif require("luasnip").expand_or_locally_jumpable() then
+			elseif require("luasnip").expand_or_jumpable then
 				require("luasnip").expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
