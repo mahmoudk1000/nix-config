f6981daecd3d66ca79404d5d7f722c25eddd57c9
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-10-08 10:02:24 +0300

    neovim: enable relative line

--- modules/programs/neovim/init.lua
+++ modules/programs/neovim/init.lua
@@ -30,6 +30,7 @@ vim.wo.signcolumn = "yes"
 vim.o.autoindent = true
 vim.o.clipboard = "unnamedplus"
 vim.o.expandtab = true
+vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
