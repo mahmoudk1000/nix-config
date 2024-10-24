55a748e8a69f4ad750aad97f44978c59a4d48115
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-10-24 12:41:35 +0300

    tmux: revamp

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -1,17 +1,18 @@
-{ pkgs, theme, ... }:
+{ pkgs, ... }:
 
 {
   programs.tmux = {
     enable = true;
     prefix = "C-Space";
     baseIndex = 1;
-    escapeTime = 20;
+    escapeTime = 5;
     shell = "${pkgs.zsh}/bin/zsh";
-    terminal = "xterm-256color";
+    terminal = "screen-256color";
     keyMode = "vi";
     historyLimit = 10000;
     mouse = true;
     plugins = with pkgs.tmuxPlugins; [
+      yank
       fingers
       better-mouse-mode
     ];
@@ -22,7 +23,9 @@
 
       bind r source-file ~/.config/tmux/tmux.conf
 
+      set -g renumber-windows on
       set-option -g focus-events on
+      set-option -ga terminal-overrides ',screen-256color:Tc'
 
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?\.?(view|n?vim?x?)(-wrapped)?(diff)?$'"
 
@@ -51,44 +54,47 @@
       bind-key "{" split-window -h -c "#{pane_current_path}"
       bind-key "}" split-window -v -c "#{pane_current_path}"
 
+      bind-key r command-prompt "rename-window %%"
       bind-key s choose-session
       bind-key c new-window -c "#{pane_current_path}"
 
       bind-key ] swap-window -t +1
       bind-key [ swap-window -t -1
 
+      # Copy/Paste
       bind-key c copy-mode
+      unbind p
+      bind p paste-buffer
+      bind P choose-buffer
+
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xclip -in -selection clipboard"
+      bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
 
       # Statusline Hide/Unhide
       bind -n M-down set -q status off
       bind -n M-up set -q status on
 
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-      bind-key -T copy-mode-vi y send-keys -X copy-pipe "xclip -i -sel p -f | xclip -i -sel c "
-      bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
-      bind P paste-buffer
-
-      set-window-option -g allow-rename off
-
-      set -g pane-border-style fg=${theme.base02}
-      set -g pane-active-border-style fg=${theme.base0A}
+      # Appearance
+      set -g pane-border-style fg=white,bg=default
+      set -g pane-active-border-style fg=white,bg=default
 
       set-option -g status-justify absolute-centre
       set-option -g status-position bottom
 
-      set -g window-status-format         "#[fg=cyan,bg=default]▒▓█#[fg=black,bg=cyan]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]█▓▒"
-      set -g window-status-current-format "#[fg=blue,bg=default]▒▓█#[fg=black,bg=blue]#I #[bg=brightblack,fg=white] #W#[fg=brightblack,bg=default]█▓▒"
+      set -g message-style bg=black,fg=yellow
+      set -g status-style bg=black
 
-      set -g status-style "bg=${theme.base00}"
-      set -ag status-style "fg=${theme.base01}"
+      setw -g window-status-format          "#[fg=white,bg=brightblack] #I #[fg=white,bg=black] #W "
+      setw -g window-status-current-format  "#[fg=white,bg=blue] #I #[fg=white,bg=brightblack] #W "
 
       # Status Right
       set -g status-right-length 70
-      set -g status-right "#[bg=default,fg=magenta]▒▓█#[fg=white,bg=brightblack] %a, %d %b #[fg=black,bg=brightblack]• #[fg=magenta,bg=brightblack,bold]%R#[fg=brightblack,bg=default]█▓▒"
+      set -g status-right "#[fg=white,bg=magenta] %b %d %Y #[fg=white,bg=blue] %R "
 
       # Status Left
-      set -g status-left-length 70
-      set -g status-left "#[bg=default,fg=red]▒▓█#[fg=white,bg=brightblack] #(mpc current | sed 's/-/~/')#[fg=brightblack,bg=default]█▓▒"
+      set -g status-left-length 0
+      set -g status-left ""
     '';
   };
 }
