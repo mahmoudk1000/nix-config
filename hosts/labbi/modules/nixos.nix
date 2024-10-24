8e9cf85f723f2a7d6d7824be557f8490241e9312
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-10-24 22:59:23 +0300

    hosts.labbi: add nixos-unstable cachix

--- hosts/labbi/modules/nixos.nix
+++ hosts/labbi/modules/nixos.nix
@@ -16,8 +16,14 @@
         "root"
         "@wheel"
       ];
-      substituters = [ "https://cache.garnix.io" ];
-      trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cache.garnix.io"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
     };
     gc = {
       automatic = true;
