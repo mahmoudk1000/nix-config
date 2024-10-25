5b96f1655dfc84b492900802158fb6c17dbfada6
Mahmoud Ayman <mahmoudk1000@gmail.com>
2024-10-26 01:17:41 +0300

    neovim: increase format timeout

--- modules/programs/neovim/config/conform.lua
+++ modules/programs/neovim/config/conform.lua
@@ -15,6 +15,10 @@ require("conform").setup({
 		yaml = { "yamlfix" },
 		tex = { "latexindent" },
 	},
+	format_on_save = {
+		timeout_ms = 1000,
+		lsp_format = "fallback",
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
