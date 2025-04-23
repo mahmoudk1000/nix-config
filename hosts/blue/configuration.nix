a3da88dea5c40b453794e66b52a705ec3923ee84
Mahmoud Ayman <mahmoudk1000@gmail.com>
2025-04-23 17:48:59 +0000

    hosts(blue): update

--- hosts/blue/configuration.nix
+++ hosts/blue/configuration.nix
@@ -5,16 +5,167 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ config, lib, pkgs, host, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  host,
+  inputs,
+  ...
+}:
 
 {
-  imports = [
-    # include NixOS-WSL modules
-  ];
+  imports = [ ];
 
-  wsl.enable = true;
-  wsl.defaultUser = "${host.username}";
+  age = {
+    identityPaths = [
+      "/home/${host.username}/.ssh/id_ed25519"
+    ];
+    secrets = {
+      "users/${host.username}".file = ../../secrets/users/farouk.age;
+    };
+  };
+
+  users.users = {
+    root.hashedPasswordFile = config.age.secrets."users/${host.username}".path;
+    ${host.username} = {
+      description = "Mahmoud Farouk";
+      isNormalUser = true;
+      home = "/home/${host.username}";
+      shell = pkgs.zsh;
+      extraGroups = [
+        "wheel"
+        "audio"
+        "users"
+        "tty"
+        "input"
+        "disk"
+        (lib.mkIf config.virtualisation.docker.enable "docker")
+      ];
+      hashedPasswordFile = config.age.secrets."users/${host.username}".path;
+    };
+  };
+
+  wsl = {
+    enable = true;
+    defaultUser = "${host.username}";
+    startMenuLaunchers = true;
+    docker-desktop.enable = true;
+    wslConf = {
+      interop = {
+        enabled = false;
+        appendWindowsPath = false;
+      };
+      automount.root = "/mnt";
+      network = {
+        generateHosts = false;
+      };
+    };
+  };
+
+  programs = {
+    zsh.enable = true;
+  };
+
+  virtualisation = {
+    docker = {
+      enable = true;
+    };
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  boot.isContainer = true;
+
   networking.hostName = "${host.hostName}";
+
+  documentation = {
+    dev.enable = true;
+    man.generateCaches = true;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Disable systemd units that don't make sense on WSL
+  systemd.services."serial-getty@ttyS0".enable = false;
+  systemd.services."serial-getty@hvc0".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@".enable = false;
+
+  systemd.services.firewall.enable = false;
+  systemd.services.systemd-resolved.enable = false;
+  systemd.services.systemd-udevd.enable = false;
+
+  # Don't allow emergency mode, because we don't have a console.
+  systemd.enableEmergencyMode = false;
+
+  environment = {
+    pathsToLink = [ "/share/zsh" ];
+    shells = [ config.users.users.${host.username}.shell ];
+    systemPackages = with pkgs; [
+      agenix
+      vim
+      wget
+      git
+      wslu
+      wsl-open
+      man-pages
+    ];
+  };
+
+  # Nix
+  nix = {
+    package = pkgs.nixVersions.latest;
+    daemonIOSchedClass = "idle";
+    daemonCPUSchedPolicy = "idle";
+    daemonIOSchedPriority = 7;
+    settings = {
+      keep-outputs = false;
+      keep-derivations = false;
+      auto-optimise-store = true;
+      builders-use-substitutes = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://cache.garnix.io"
+        "https://fortuneteller2k.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
+      ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+    registry.nixpkgs.flake = inputs.nixpkgs;
+  };
+
+  # NixOS
+  system = {
+    switch = {
+      enable = false;
+      enableNg = true;
+    };
+    autoUpgrade = {
+      enable = false;
+      channel = "https://nixos.org/channels/nixos-unstable";
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
---
+++ modules/programs/wsl.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./neovim
+    ./bash.nix
+    ./devops.nix
+    ./fetch.nix
+    ./git.nix
+    ./gnu.nix
+    ./newsboat.nix
+    ./starship.nix
+    ./texlive.nix
+    ./tmux.nix
+    ./zsh.nix
+  ];
+}
