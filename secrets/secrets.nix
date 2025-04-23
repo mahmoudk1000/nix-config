549061fd42cbad1ba060528fa8372cb6463ef52e
Mahmoud Ayman <mahmoudk1000@gmail.com>
2025-04-23 17:54:14 +0000

    secrets: init work profile

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   users = {
     mahmoud = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcSp0c90xKLAua7JAqhqZkueVTpDcKhOm1AdeFoyFTX";
+    farouk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGLdoOZLtDMy5O+ed8TMUvxY8QW1T3gkl7BqtgMGDVG ww930\a930850@DESKTOP-J3DV8C4";
   };
 
   hosts = {
@@ -11,10 +12,15 @@ let
     users.mahmoud
     hosts.labbi
   ];
+
+  work = [
+    users.farouk
+  ];
 in
 {
   # Users
   "users/mahmoud.age".publicKeys = default;
+  "users/farouk.age".publicKeys = work;
 
   # Accounts
   "accounts/personal.age".publicKeys = default;
