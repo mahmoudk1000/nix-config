e169f2d1afd17087713593f98ca3b8752633977e
Mahmoud Ayman <mahmoudk1000@gmail.com>
2025-05-03 17:44:39 +0300

    programs(vscode): update options

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -9,226 +9,230 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium-fhs;
-    enableUpdateCheck = false;
-    extensions =
-      with pkgs.vscode-extensions;
-      [
-        antfu.icons-carbon
-        github.vscode-pull-request-github
-        usernamehw.errorlens
-        foxundermoon.shell-format
-        arrterian.nix-env-selector
-        kamikillerto.vscode-colorize
-        ms-azuretools.vscode-docker
-        ms-python.python
-        redhat.java
-        redhat.vscode-yaml
-        ritwickdey.liveserver
-        esbenp.prettier-vscode
-        davidlday.languagetool-linter
-        mads-hartmann.bash-ide-vscode
-        pkief.material-icon-theme
-        davidanson.vscode-markdownlint
-        yzhang.markdown-all-in-one
-        shardulm94.trailing-spaces
-        streetsidesoftware.code-spell-checker
-        ms-ceintl.vscode-language-pack-de
-        bbenoist.nix
-        ms-python.vscode-pylance
-        hashicorp.terraform
-        jnoortheen.nix-ide
-        vscodevim.vim
-        visualstudioexptteam.vscodeintellicode
-        github.copilot
-        github.copilot-chat
-        ms-vscode.powershell
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "fluent-icons";
-          publisher = "miguelsolorio";
-          version = "0.0.18";
-          sha256 = "sha256-sE0A441QPwokBoLoCqtImDHmlAXd66fj8zsJR7Ci+Qs=t";
-        }
-      ];
-    keybindings = import ./keybind.nix;
-    userSettings = {
-      editor = {
-        fontSize = 12;
-        fontFamily = [
-          font.name
-          font.nameTerm
-        ];
-        fontLigatures = true;
-        acceptSuggestionOnEnter = "off";
-        autoClosingBrackets = "always";
-        cursorBlinking = "smooth";
-        cursorSmoothCaretAnimation = true;
-        formatOnPaste = true;
-        formatOnSave = true;
-        formatOnType = true;
-        renderFinalNewline = false;
-        rulers = [ 80 ];
-        tabSize = 4;
-        smoothScrolling = true;
-        stickyTabStops = true;
-        wordWrap = "on";
-        bracketPairColorization = {
-          enabled = true;
-        };
-        guides = {
-          bracketPairs = "active";
-        };
-      };
-
-      # Window
-      window = {
-        zoomLevel = -0.5;
-        menuBarVisibility = "toggle";
-        titleBarStyle = "native";
-      };
-
-      # Workbench
-      workbench = {
-        colorTheme = "Mont";
-        iconTheme = "eq-material-theme-icons-light";
-        productIconTheme = "fluent-icons";
-        editor = {
-          showTabs = true;
-        };
-      };
-
-      # Terminal
-      terminal = {
-        integrated = {
-          fontFamily = font.nameTerm;
-          fontSize = 12;
-          allowChords = false;
-          gpuAcceleration = "on";
-          cursorStyle = "line";
-          cursorBlinking = true;
-        };
-      };
-
-      # Files
-      files = {
-        autoSave = "onFocusChange";
-        trimTrailingWhitespace = true;
-        trimFinalNewlines = true;
-        exclude = {
-          "**/.classpath" = true;
-          "**/.project" = true;
-          "**/.settings" = true;
-          "**/.factorypath" = true;
-        };
-      };
-
-      # Telemetry
-      telemetry = {
-        enableCrashReporter = false;
-        enableTelemetry = false;
-        telemetryLevel = "off";
-      };
-      redhat = {
-        telemetry = {
-          enabled = false;
-        };
-      };
-      githubPullRequests = {
-        telemetry = {
-          enabled = false;
-        };
-      };
-
-      # Updates
-      update = {
-        mode = "none";
-      };
-      extensions = {
-        autoUpdate = false;
-        ignoreRecommendations = true;
-      };
-
-      # Git
-      git = {
-        autofetch = true;
-      };
-
-      # Python
-      python = {
-        languageServer = "Pylance";
-        analysis = {
-          autoImportCompletions = true;
-          typeCheckingMode = "basic";
-        };
-        linting = {
-          enabled = true;
-          pylintEnabled = true;
-          pylintPath = "${pkgs.python3Packages.pylint}/bin/pylint";
-        };
-      };
-
-      # vim
-      vim = {
-        useSystemClipboard = true;
-      };
-
-      # Languages
-      ## Nix
-      nix = {
-        enableLanguageServer = true;
-        nixEnvSelector = {
-          nixFile = "\${workspaceRoot}/shell.nix";
-        };
-        serverPath = "${pkgs.nixd}/bin/nixd";
-        serverSettings = {
-          nixd = {
-            eval = { };
-            formatting = {
-              command = [ "nixpkgs-fmt" ];
+    profiles = {
+      default = {
+        enableUpdateCheck = false;
+        extensions =
+          with pkgs.vscode-extensions;
+          [
+            antfu.icons-carbon
+            github.vscode-pull-request-github
+            usernamehw.errorlens
+            foxundermoon.shell-format
+            arrterian.nix-env-selector
+            kamikillerto.vscode-colorize
+            ms-azuretools.vscode-docker
+            ms-python.python
+            redhat.java
+            redhat.vscode-yaml
+            ritwickdey.liveserver
+            esbenp.prettier-vscode
+            davidlday.languagetool-linter
+            mads-hartmann.bash-ide-vscode
+            pkief.material-icon-theme
+            davidanson.vscode-markdownlint
+            yzhang.markdown-all-in-one
+            shardulm94.trailing-spaces
+            streetsidesoftware.code-spell-checker
+            ms-ceintl.vscode-language-pack-de
+            bbenoist.nix
+            ms-python.vscode-pylance
+            hashicorp.terraform
+            jnoortheen.nix-ide
+            vscodevim.vim
+            visualstudioexptteam.vscodeintellicode
+            github.copilot
+            github.copilot-chat
+            ms-vscode.powershell
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "fluent-icons";
+              publisher = "miguelsolorio";
+              version = "0.0.18";
+              sha256 = "sha256-sE0A441QPwokBoLoCqtImDHmlAXd66fj8zsJR7Ci+Qs=t";
+            }
+          ];
+        keybindings = import ./keybind.nix;
+        userSettings = {
+          editor = {
+            fontSize = 12;
+            fontFamily = [
+              font.name
+              font.nameTerm
+            ];
+            fontLigatures = true;
+            acceptSuggestionOnEnter = "off";
+            autoClosingBrackets = "always";
+            cursorBlinking = "smooth";
+            cursorSmoothCaretAnimation = true;
+            formatOnPaste = true;
+            formatOnSave = true;
+            formatOnType = true;
+            renderFinalNewline = false;
+            rulers = [ 80 ];
+            tabSize = 4;
+            smoothScrolling = true;
+            stickyTabStops = true;
+            wordWrap = "on";
+            bracketPairColorization = {
+              enabled = true;
             };
-            options = {
-              enable = true;
-              target = {
-                args = [ ];
-                installable = "<flakeref>#nixosConfigurations.<name>.options";
+            guides = {
+              bracketPairs = "active";
+            };
+          };
+
+          # Window
+          window = {
+            zoomLevel = -0.5;
+            menuBarVisibility = "toggle";
+            titleBarStyle = "native";
+          };
+
+          # Workbench
+          workbench = {
+            colorTheme = "Mont";
+            iconTheme = "eq-material-theme-icons-light";
+            productIconTheme = "fluent-icons";
+            editor = {
+              showTabs = true;
+            };
+          };
+
+          # Terminal
+          terminal = {
+            integrated = {
+              fontFamily = font.nameTerm;
+              fontSize = 12;
+              allowChords = false;
+              gpuAcceleration = "on";
+              cursorStyle = "line";
+              cursorBlinking = true;
+            };
+          };
+
+          # Files
+          files = {
+            autoSave = "onFocusChange";
+            trimTrailingWhitespace = true;
+            trimFinalNewlines = true;
+            exclude = {
+              "**/.classpath" = true;
+              "**/.project" = true;
+              "**/.settings" = true;
+              "**/.factorypath" = true;
+            };
+          };
+
+          # Telemetry
+          telemetry = {
+            enableCrashReporter = false;
+            enableTelemetry = false;
+            telemetryLevel = "off";
+          };
+          redhat = {
+            telemetry = {
+              enabled = false;
+            };
+          };
+          githubPullRequests = {
+            telemetry = {
+              enabled = false;
+            };
+          };
+
+          # Updates
+          update = {
+            mode = "none";
+          };
+          extensions = {
+            autoUpdate = false;
+            ignoreRecommendations = true;
+          };
+
+          # Git
+          git = {
+            autofetch = true;
+          };
+
+          # Python
+          python = {
+            languageServer = "Pylance";
+            analysis = {
+              autoImportCompletions = true;
+              typeCheckingMode = "basic";
+            };
+            linting = {
+              enabled = true;
+              pylintEnabled = true;
+              pylintPath = "${pkgs.python3Packages.pylint}/bin/pylint";
+            };
+          };
+
+          # vim
+          vim = {
+            useSystemClipboard = true;
+          };
+
+          # Languages
+          ## Nix
+          nix = {
+            enableLanguageServer = true;
+            nixEnvSelector = {
+              nixFile = "\${workspaceRoot}/shell.nix";
+            };
+            serverPath = "${pkgs.nixd}/bin/nixd";
+            serverSettings = {
+              nixd = {
+                eval = { };
+                formatting = {
+                  command = [ "nixpkgs-fmt" ];
+                };
+                options = {
+                  enable = true;
+                  target = {
+                    args = [ ];
+                    installable = "<flakeref>#nixosConfigurations.<name>.options";
+                  };
+                };
               };
             };
           };
+          ## java
+          groovyLint.java.executable = "groovy-language-server";
+          java = {
+            configuration.runtimes = [
+              {
+                name = "JavaSE-17";
+                path = "${pkgs.jdk}/lib/openjdk";
+                default = true;
+              }
+            ];
+            jdt.ls.java.home = "${pkgs.jdk}/lib/openjdk";
+            format.settings.profile = "GoogleStyle";
+          };
+          ## Docker
+          dockerfile = {
+            editor.defaultFormatter = "ms-azuretools.vscode-docker";
+          };
+          ## Web
+          "[html][javascript][javascriptreact][json][typescript][typescriptreact][scss]" = {
+            editor.defaultFormatter = "esbenp.prettier-vscode";
+          };
+          ## Bash
+          shellscript = {
+            editor = {
+              tabSize = 4;
+              insertSpaces = false;
+            };
+            shellformat.path = "${pkgs.shfmt}/bin/shfmt";
+          };
+          ## C, CPP
+          "[cpp][c]" = {
+            editor.defaultFormatter = "xaver.clang-format";
+          };
         };
-      };
-      ## java
-      groovyLint.java.executable = "groovy-language-server";
-      java = {
-        configuration.runtimes = [
-          {
-            name = "JavaSE-17";
-            path = "${pkgs.jdk}/lib/openjdk";
-            default = true;
-          }
-        ];
-        jdt.ls.java.home = "${pkgs.jdk}/lib/openjdk";
-        format.settings.profile = "GoogleStyle";
-      };
-      ## Docker
-      dockerfile = {
-        editor.defaultFormatter = "ms-azuretools.vscode-docker";
-      };
-      ## Web
-      "[html][javascript][javascriptreact][json][typescript][typescriptreact][scss]" = {
-        editor.defaultFormatter = "esbenp.prettier-vscode";
-      };
-      ## Bash
-      shellscript = {
-        editor = {
-          tabSize = 4;
-          insertSpaces = false;
-        };
-        shellformat.path = "${pkgs.shfmt}/bin/shfmt";
-      };
-      ## C, CPP
-      "[cpp][c]" = {
-        editor.defaultFormatter = "xaver.clang-format";
       };
     };
   };
