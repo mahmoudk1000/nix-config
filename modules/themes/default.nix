fde4dba5aec4ed0e8c9b10da30aaf55f1e7d531c
Mahmoud Ayman <mahmoudk1000@gmail.com>
2025-05-04 16:42:26 +0300

    theme: un hardvalued

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -1,27 +1,42 @@
+{
+  theme ? null,
+}:
+
 let
-  blaulicht = import ./blaulicht.nix;
-  blind = import ./blind.nix;
-  bliss = import ./bliss.nix;
-  bluewery = import ./bluewery.nix;
-  coral = import ./coral.nix;
-  crystal = import ./crystal.nix;
-  fujin = import ./fuji.nix;
-  iceberg = import ./iceberg.nix;
-  idx = import ./idx.nix;
-  jotai = import ./jotai.nix;
-  kalt = import ./kalt.nix;
-  late = import ./late.nix;
-  melle = import ./melle.nix;
-  mongo = import ./mongo.nix;
-  mountain = import ./mountain.nix;
-  night = import ./night.nix;
-  oxo = import ./oxo.nix;
-  poimandres = import ./poimandres.nix;
-  rnvy = import ./rnvy.nix;
-  rogue = import ./rogue.nix;
-  seaul = import ./seaul.nix;
-  tokyonight = import ./tokyonight.nix;
-  uwu = import ./uwu.nix;
-  yuto = import ./yuro.nix;
+  themeGallery = {
+    blaulicht = import ./blaulicht.nix;
+    blind = import ./blind.nix;
+    bliss = import ./bliss.nix;
+    bluewery = import ./bluewery.nix;
+    coral = import ./coral.nix;
+    crystal = import ./crystal.nix;
+    fujin = import ./fuji.nix;
+    iceberg = import ./iceberg.nix;
+    idx = import ./idx.nix;
+    jotai = import ./jotai.nix;
+    kalt = import ./kalt.nix;
+    late = import ./late.nix;
+    melle = import ./melle.nix;
+    momo = import ./momo.nix;
+    mongo = import ./mongo.nix;
+    mountain = import ./mountain.nix;
+    night = import ./night.nix;
+    oxo = import ./oxo.nix;
+    poimandres = import ./poimandres.nix;
+    rnvy = import ./rnvy.nix;
+    rogue = import ./rogue.nix;
+    seaul = import ./seaul.nix;
+    tokyonight = import ./tokyonight.nix;
+    uwu = import ./uwu.nix;
+    yuto = import ./yuro.nix;
+  };
+
+  validThemes = builtins.attrNames themeGallery;
+
 in
-melle
+if theme == null then
+  throw "Error: No theme specified. Please choose one of the following themes: ${builtins.concatStringsSep ", " validThemes}"
+else if !builtins.elem theme validThemes then
+  throw "Error: Invalid theme '${theme}'. Valid themes are: ${builtins.concatStringsSep ", " validThemes}"
+else
+  builtins.getAttr theme themeGallery
---
+++ modules/themes/momo.nix
@@ -0,0 +1,20 @@
+{
+  base00 = "#090C10";
+  base01 = "#2c3642";
+  base02 = "#11151A";
+  base03 = "#d6bfc2";
+  base04 = "#b6d4c1";
+  base05 = "#e8ddb2";
+  base06 = "#6a849a";
+  base07 = "#c3b4dc";
+  base08 = "#a6d7d8";
+  base09 = "#cdd3d9";
+  base0A = "#101820";
+  base0B = "#e2c4c6";
+  base0C = "#c3dfd1";
+  base0D = "#e6e3c0";
+  base0E = "#8aa3bd";
+  base0F = "#d8c4e4";
+  base0G = "#b7e0e2";
+  base0H = "#e4e8ec";
+}
