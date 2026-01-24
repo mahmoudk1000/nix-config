b65e3e41b12b2ac7826277848978486ba88aad2b
Mahmoud Ayman <mahmoudk1000@gmail.com>
2026-01-24 14:58:40 +0200

    secrets: update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,30 +1,17 @@
 let
   users = {
     mahmoud = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcSp0c90xKLAua7JAqhqZkueVTpDcKhOm1AdeFoyFTX";
-    farouk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGLdoOZLtDMy5O+ed8TMUvxY8QW1T3gkl7BqtgMGDVG ww930\a930850@DESKTOP-J3DV8C4";
+    farouk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGLdoOZLtDMy5O+ed8TMUvxY8QW1T3gkl7BqtgMGDVG";
   };
-
-  hosts = {
-    labbi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcSp0c90xKLAua7JAqhqZkueVTpDcKhOm1AdeFoyFTX";
-  };
-
-  default = [
-    users.mahmoud
-    hosts.labbi
-  ];
-
-  work = [
-    users.farouk
-  ];
 in
 {
   # Users
-  "users/mahmoud.age".publicKeys = default;
-  "users/farouk.age".publicKeys = work;
+  "users/mahmoud.age".publicKeys = users.mahmoud;
+  "users/farouk.age".publicKeys = users.farouk;
 
   # Accounts
-  "accounts/personal.age".publicKeys = default;
-  "accounts/secondary.age".publicKeys = default;
-  "accounts/work.age".publicKeys = default;
-  "accounts/uni.age".publicKeys = default;
+  "accounts/personal.age".publicKeys = users.mahmoud;
+  "accounts/secondary.age".publicKeys = users.mahmoud;
+  "accounts/work.age".publicKeys = users.mahmoud;
+  "accounts/uni.age".publicKeys = users.mahmoud;
 }
