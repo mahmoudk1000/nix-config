d71538eca0ccfca2e0b84fbb3f32d7614fbef038
Mahmoud Ayman <mahmoudk1000@gmail.com>
2026-03-23 00:16:49 +0200

    hosts(common): remove java

--- hosts/common/system/programs.nix
+++ hosts/common/system/programs.nix
@@ -1,14 +1,8 @@
-{ pkgs, ... }:
-
 {
   programs = {
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
-    };
-    java = {
-      enable = true;
-      package = pkgs.jdk;
     };
     zsh = {
       enable = true;
