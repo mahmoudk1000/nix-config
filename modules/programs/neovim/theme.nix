d9ca854aa7b3d54b0e597c01ab98b58c270d7af7
Mahmoud <mahmoudk1000@gmail.com>
2026-09-22 14:03:54 +0300

    programs(neovim): remap terminal colors

--- modules/programs/neovim/theme.nix
+++ modules/programs/neovim/theme.nix
@@ -539,20 +539,20 @@ in
 
     -- Terminal colors
     vim.g.terminal_color_0 = c.base01
-    vim.g.terminal_color_1 = c.base0B
-    vim.g.terminal_color_2 = c.base0E
-    vim.g.terminal_color_3 = c.base0D
-    vim.g.terminal_color_4 = c.base09
+    vim.g.terminal_color_1 = c.base08
+    vim.g.terminal_color_2 = c.base0B
+    vim.g.terminal_color_3 = c.base09
+    vim.g.terminal_color_4 = c.base0D
     vim.g.terminal_color_5 = c.base0F
-    vim.g.terminal_color_6 = c.base08
-    vim.g.terminal_color_7 = c.base05
-    vim.g.terminal_color_8 = c.base03
-    vim.g.terminal_color_9 = c.base0B
-    vim.g.terminal_color_10 = c.base0E
-    vim.g.terminal_color_11 = c.base0D
-    vim.g.terminal_color_12 = c.base09
+    vim.g.terminal_color_6 = c.base0E
+    vim.g.terminal_color_7 = c.base07
+    vim.g.terminal_color_8 = c.base02
+    vim.g.terminal_color_9 = c.base08
+    vim.g.terminal_color_10 = c.base0B
+    vim.g.terminal_color_11 = c.base09
+    vim.g.terminal_color_12 = c.base0D
     vim.g.terminal_color_13 = c.base0F
-    vim.g.terminal_color_14 = c.base07
+    vim.g.terminal_color_14 = c.base0E
     vim.g.terminal_color_15 = c.base06
   '';
 }
